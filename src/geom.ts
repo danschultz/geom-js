aefff877f803e4f1e413719000d8aa9dfcb62d54
Dan Schultz <schultz.t.dan@gmail.com>
2016-04-21 01:44:39 -0700

    Rip out jspm

--- src/geom.ts
+++ src/geom.ts
@@ -1,3 +1,3 @@
-export {default as Interval} from "./interval";
-export {default as Point} from "./point";
-export {default as Rect} from "./rect";
+export {default as Interval} from "./geom/interval";
+export {default as Point} from "./geom/point";
+export {default as Rect} from "./geom/rect";
